1a72bf07393abf16041b16ba3af96eb860c0a6ed
Viktor Voronin <vvoronin@web3tech.ru>
2020-02-28 14:14:48 +0300

    refactor build

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,73 @@
 const path = require('path');
+const merge = require('webpack-merge'); // делает deep merge конфигов
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-const main = (name, minimize) => ({
-    entry: './src/Signer.ts',
-    mode: "production",
+const NODE_ENV = process.env.NODE_ENV
+
+const PATHS = {
+    root: path.resolve(__dirname),
+    dist: path.resolve('./dist')
+}
+
+const commonConfig = merge([{
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
+                loader: 'ts-loader',
+                exclude: [
+                    /node_modules/,
+                    path.join(PATHS.root, 'test')
+                ],
+                options: {
+                    transpileOnly: true
+                }
             },
         ],
     },
-    optimization: {
-        minimize,
-        usedExports: true
-    },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+        modules: ['node_modules'],
     },
     output: {
         library: 'signer',
-        libraryTarget: "umd",
-        globalObject: "this",
-        filename: name,
-        path: path.resolve(__dirname, 'dist'),
-    }
-});
-
-module.exports = [
-    {
-        ...main('signer.js', false),
-        devtool: 'inline-source-map',
-        mode: "development",
+        libraryTarget: 'umd',
+        globalObject: 'this',
+        path: PATHS.dist,
+        filename: '[name].js'
     },
-    {
-        ...main('signer.min.js', true)
+    plugins: [
+        new ForkTsCheckerWebpackPlugin({
+            tsconfig: path.join(PATHS.root, 'tsconfig.build.json')
+        })
+    ]
+}]);
+
+module.exports = () => {
+    const mode = NODE_ENV
+
+    if (mode === 'production') {
+        return merge([
+            commonConfig,
+            { entry:
+                {
+                    'signer.min': path.join(PATHS.root, 'src', 'index.ts')
+                },
+            },
+            { mode }
+        ]);
+    } else if (mode === 'development') {
+        return merge([
+            commonConfig,
+            { entry:
+                {
+                    'signer': path.join(PATHS.root, 'src', 'index.ts')
+                },
+            },
+            { devtool: 'inline-source-map' },
+            { mode },
+        ]);
+    } else {
+        throw new Error(`Unexpected mode provided: ${mode}. Must be one of ['development', 'production']`);
     }
-];
+};
