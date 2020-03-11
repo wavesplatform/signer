2572a201d53b963ef72b4361de58795eec3a6e28
Viktor Voronin <vvoronin@web3tech.ru>
2020-03-11 14:41:49 +0300

    Delete old tests

--- test/utils.ts
+++
@@ -1,9 +0,0 @@
-import { TTransactionFromAPI } from '@waves/ts-types';
-import { TLong } from '../src/interface';
-import { waitForTx } from '@waves/waves-transactions';
-import { NODE_URL } from './_state';
-
-export const wait = (tx: TTransactionFromAPI<TLong>) => waitForTx(tx.id, { apiBase: NODE_URL })
-    .then(() => tx);
-
-export const waitTime = (time: number) => new Promise(resolve => setTimeout(resolve, time));
